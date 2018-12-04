0dd05b2c1099d9a7cd9099249af944be10a4b353
balexvic <balexvicx@gmail.com>
2018-12-04 03:44:32 +0100

    2

--- libs/user.js
+++ libs/user.js
@@ -19,7 +19,7 @@ userSchema.virtual('password')
   this._plainPassword = password;
   if (password) {
     this.salt = crypto.randomBytes(128).toString('base64');
-    this.passwordHash = crypto.pbkdf2Sync(password, this.salt, 1, 128, 'HS256').toString('base64');
+    this.passwordHash = crypto.pbkdf2Sync(password, this.salt, 1, 128, 'sha1').toString('base64');
   } else {
     this.salt = undefined;
     this.passwordHash = undefined;
@@ -34,7 +34,7 @@ userSchema.methods.checkPassword = function (password) {
   if (!password) return false;
   if (!this.passwordHash) return false;
 
-  return crypto.pbkdf2Sync(password, this.salt, 1, 128, 'HS256').toString('base64') == this.passwordHash;
+  return crypto.pbkdf2Sync(password, this.salt, 1, 128, 'sha1').toString('base64') == this.passwordHash;
 };
 
 
